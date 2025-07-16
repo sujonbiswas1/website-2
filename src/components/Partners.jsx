30f055d9774bd0331afcae0706a400c4f8a29e03
sujonbiswas1 <nextgenprogrammer1@gmail.com>
2025-07-16 06:37:00 +0600

    first commit

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import Marquee from "react-fast-marquee";
 const Partners = () => {
   return (
-    <section className='py-6 px-8 sm:px-10 md:px-14 lg:px-20 max-w-[1440px] mx-auto '>
+    <section className='py-6 px-8 sm:px-10 md:px-14 lg:px-20 max-w-[1440px] mx-auto'>
         <div className='text-center text-primary'>
             <h3 className='font-montserrat tracking-widest'>our trusted partners</h3>
 
-            <div className='py-10 flex justify-center'>
+            <div className='py-12 flex justify-center'>
                 <Marquee>
-                      <img src="/logos.png" alt="logo" />
+                      <img src="/logos.png" alt="logos" />
                 </Marquee>
               
 
